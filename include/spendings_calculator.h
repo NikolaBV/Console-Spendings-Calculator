bfbd9672ecefa8604a3dd7a50fa281292c836bf8
NikolaBV <bulgarianmapper64@gmail.com>
2023-11-09 00:59:31 +0200

    Looks like it works at first glance

--- include/spendings_calculator.h
+++ include/spendings_calculator.h
@@ -12,8 +12,10 @@ class SpendingsCalculator {
         bool doesDayCSVFileExist(const std::string& filename);
         void CreateCSVFile(const std::string& filename);
         bool isCSVFileEmpty(std::string& filename);
+        bool isValidInputFormat(const std::string& input);
         void menuForCSVFileActions(std::string& filename);
         void editCSVFile(std::string& filename);
         void printContentsOfCSVFile(std::string& filename);
+        
 };
 #endif // SPENDINGS_CALCULATOR_H
